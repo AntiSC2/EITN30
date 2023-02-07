8653f40f44d1fa0a5489e98b1bf39fddc5c8cf11
madeleineberild <madeleine.berild@hotmail.com>
2023-02-07 14:58:14 +0100

    Test some manipulations

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,3 @@
-/**
- * A simple example of sending data from 1 nRF24L01 transceiver to another.
- *
- * This example was written to be used on 2 devices acting as "nodes".
- * Use `ctrl+c` to quit at any time.
- */
 #include <ctime>       // time()
 #include <iostream>    // cin, cout, endl
 #include <string>      // string, getline()
@@ -12,22 +6,7 @@
 
 using namespace std;
 
-/****************** Linux ***********************/
-// Radio CE Pin, CSN Pin, SPI Speed
-// CE Pin uses GPIO number with BCM and SPIDEV drivers, other platforms use their own pin numbering
-// CS Pin addresses the SPI bus number at /dev/spidev<a>.<b>
-// ie: RF24 radio(<ce_pin>, <a>*10+<b>); spidev1.0 is 10, spidev1.1 is 11 etc..
-
-// Generic:
 RF24 radio(17, 0);
-/****************** Linux (BBB,x86,etc) ***********************/
-// See http://nRF24.github.io/RF24/pages.html for more information on usage
-// See http://iotdk.intel.com/docs/master/mraa/ for more information on MRAA
-// See https://www.kernel.org/doc/Documentation/spi/spidev for more information on SPIDEV
-
-// For this example, we'll be using a payload containing
-// a single float number that will be incremented
-// on every successful transmission
 float payload = 0.0;
 
 void setRole(); // prototype to set the node's role
@@ -47,8 +26,6 @@ int main(int argc, char** argv)
         return 0; // quit now
     }
 
-    // to use different addresses on a pair of radios, we need a variable to
-    // uniquely identify which address this radio will use to transmit
     bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
 
     // print example's name
@@ -56,23 +33,19 @@ int main(int argc, char** argv)
 
     // Let these addresses be used for the pair
     uint8_t address[2][6] = {"1Node", "2Node"};
-    // It is very helpful to think of an address as a path instead of as
-    // an identifying device destination
-
-    // Set the radioNumber via the terminal on startup
+    
     cout << "Which radio is this? Enter '0' or '1'. Defaults to '0' ";
     string input;
     getline(cin, input);
     radioNumber = input.length() > 0 && (uint8_t)input[0] == 49;
 
-    // save on transmission time by setting the radio to only transmit the
-    // number of bytes we need to transmit a float
     radio.setPayloadSize(sizeof(payload)); // float datatype occupies 4 bytes
 
-    // Set the PA Level low to try preventing power supply related problems
-    // because these examples are likely run with nodes in close proximity to
-    // each other.
     radio.setPALevel(RF24_PA_LOW); // RF24_PA_MAX is default.
+
+    radio.setDataRate(RF24_2MBPS);
+
+    radio.setAutoAck(false);
 
     // set the TX address of the RX node into the TX pipe
     radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
@@ -80,19 +53,10 @@ int main(int argc, char** argv)
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
 
-    // For debugging info
-    // radio.printDetails();       // (smaller) function that prints raw register values
-    // radio.printPrettyDetails(); // (larger) function that prints human readable data
-
-    // ready to execute program now
     setRole(); // calls master() or slave() based on user input
     return 0;
 }
 
-/**
- * set this node's role from stdin stream.
- * this only considers the first char as input.
- */
 void setRole()
 {
     string input = "";
@@ -115,9 +79,6 @@ void setRole()
     }               // while
 } // setRole()
 
-/**
- * make this node act as the transmitter
- */
 void master()
 {
     radio.stopListening(); // put radio in TX mode
@@ -147,9 +108,6 @@ void master()
     cout << failure << " failures detected. Leaving TX role." << endl;
 }
 
-/**
- * make this node act as the receiver
- */
 void slave()
 {
 
@@ -171,14 +129,8 @@ void slave()
     radio.stopListening();
 }
 
-/**
- * Calculate the ellapsed time in microseconds
- */
 uint32_t getMicros()
 {
-    // this function assumes that the timer was started using
-    // `clock_gettime(CLOCK_MONOTONIC_RAW, &startTimer);`
-
     clock_gettime(CLOCK_MONOTONIC_RAW, &endTimer);
     uint32_t seconds = endTimer.tv_sec - startTimer.tv_sec;
     uint32_t useconds = (endTimer.tv_nsec - startTimer.tv_nsec) / 1000;
