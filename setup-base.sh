08775e066f322315a149a9ab861759dfc77cda97
Jakob Sinclair <sinclair.jakob@mailbox.org>
2023-02-24 11:12:10 +0100

    Test routing

---
+++ setup-base.sh
@@ -0,0 +1,7 @@
+sudo ip tuntap add mode tun dev tun0
+sudo ip addr add 10.0.0.1/24 dev tun0
+sudo ip link set tun0 up
+
+sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+sudo iptables -A FORWARD -i wlan0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i tun0 -o wlan0 -j ACCEPT
