b29586eaaaf75951e62768cfe1d74328869d1b0d
Jakob Sinclair <sinclair.jakob@mailbox.org>
2023-02-24 14:27:45 +0100

    Change back

--- setup-base.sh
+++ setup-base.sh
@@ -2,6 +2,6 @@ sudo ip tuntap add mode tun dev tun0
 sudo ip addr add 10.0.0.1/24 peer 10.0.0.2 dev tun0
 sudo ip link set tun0 up
 
-sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 sudo iptables -A FORWARD -i wlan0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i tun0 -o wlan0 -j ACCEPT
