f2af4e41de64705ffefb33b7c94ae3573312cfdd
Jakob Sinclair <sinclair.jakob@mailbox.org>
2023-02-24 13:54:05 +0100

    Add peer

--- setup-base.sh
+++ setup-base.sh
@@ -1,5 +1,5 @@
 sudo ip tuntap add mode tun dev tun0
-sudo ip addr add 10.0.0.1/24 dev tun0
+sudo ip addr add 10.0.0.1/24 peer 10.0.0.2 dev tun0
 sudo ip link set tun0 up
 
 sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
--- setup-mobile.sh
+++ setup-mobile.sh
@@ -1,5 +1,5 @@
 sudo ip tuntap add mode tun dev tun0
-sudo ip addr add 10.0.0.2/24 dev tun0
+sudo ip addr add 10.0.0.2/24 peer 10.0.0.1 dev tun0
 sudo ip link set tun0 up
 
 sudo ip route add default via 10.0.0.1 dev tun0
