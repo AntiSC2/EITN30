53796bb266d4320d81812e69b1be3b660bda9a51
Jakob Sinclair <sinclair.jakob@mailbox.org>
2023-03-14 10:50:41 +0100

    Set up CMake with setup script

--- setup-base.sh
+++ setup-base.sh
@@ -6,3 +6,5 @@ sudo ip link set tun0 up
 sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 sudo iptables -A FORWARD -i wlan0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i tun0 -o wlan0 -j ACCEPT
+
+cmake -DCMAKE_BUILD_TYPE=Release -DMTU=1000 -DBASE=TRUE src/.
--- setup-mobile.sh
+++ setup-mobile.sh
@@ -4,3 +4,5 @@ sudo ip link set dev tun0 mtu 1000
 sudo ip link set tun0 up
 
 sudo ip route add default via 10.0.0.1 dev tun0
+
+cmake -DCMAKE_BUILD_TYPE=Release -DMTU=1000 src/.
